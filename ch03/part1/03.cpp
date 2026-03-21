d83e71ec3049f77d98e208ecf3f7769a7ae18027
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:52:55 +0900

    Update 03.cpp

--- ch03/part1/03.cpp
+++ ch03/part1/03.cpp
@@ -3,3 +3,37 @@
 // 날 짜 : 2026년 3월16일
 // 작성자 : 2500008 권오현
 // **********************************************
+
+#include <iostream>
+
+using namespace std;
+class Rectangle { 
+public:
+	int x;
+	int y;
+	int width;
+	int height;
+	int getArea(); 
+	int getPerimeter();
+	void getCoordinate();
+};
+int Rectangle::getArea() { 
+	return width * height;
+}
+int Rectangle::getPerimeter() {
+	return width + width + height + height;
+}
+void Rectangle::getCoordinate() {
+	cout << "사각형의 우측하단의 좌표는 (" << x + width << "," << y - height << ")" << endl;
+}
+int main() {
+	Rectangle rect;
+	rect.x = 1;
+	rect.y = 2;
+	rect.width = 3;
+	rect.height = 4;
+	cout << "사각형의 면적은 " << rect.getArea() << endl;
+	cout << "사각형의 둘레길이는 " << rect.getPerimeter() << endl;
+	rect.getCoordinate();
+	return 0;
+}
