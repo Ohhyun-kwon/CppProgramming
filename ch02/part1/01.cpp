bbd104b621a366b27f11d330fd00560e3cb7b3d3
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:42:53 +0900

    Update 01.cpp

--- ch02/part1/01.cpp
+++ ch02/part1/01.cpp
@@ -1,5 +1,5 @@
 // **********************************************
-// 제 목 : 문자열, * 출력 프로그램
+// 제 목 : 문제1
 // 날 짜 : 2026년 3월11일
 // 작성자 : 2500008 권오현
 // **********************************************
@@ -13,37 +13,4 @@ int main(void)
     std::cout << "학번: 201012\n";
     std::cout << "차종: 벤츠\n";
     return 0;
-}
-
-int main(void) {
-    std::cout << "군산대학교 \"홍길동\"\n";
-    std::cout << "\"축하합니다.\"\n";
-    std::cout << "100% \\취업율\\\n";
-	return 0;
-}
-
-int main(void) {
-	for (int i = 0; i < 7; i++) {
-		for (int j = 0; j < 7; j++) {
-			if (i == j || i + j == 6) {
-				std::cout << "*";
-			}
-			else {
-				std::cout << " ";
-			}
-		}
-		std::cout << "\n";
-	}
-	return 0;
-}
-
-using namespace std;
-int main(void) {
-    for (int n = 0; n < 4; n++) {
-        for (int m = 0; m <= n; m++) {
-            cout << "*";
-        }
-        cout << "\n";
-    }
-	return 0;
 }
