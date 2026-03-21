aed3303d3783c13f7e88d24e283fc96d7a8d899e
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:52:19 +0900

    Update 02.cpp

--- ch03/part1/02.cpp
+++ ch03/part1/02.cpp
@@ -3,3 +3,26 @@
 // 날 짜 : 2026년 3월16일
 // 작성자 : 2500008 권오현
 // **********************************************
+
+#include <iostream>
+
+using namespace std;
+class Sphere {
+public:
+	double radius;
+	double getArea();
+	double getVolume();
+};
+double Sphere::getVolume() {
+	return (4.0/3.0) * 3.14 * radius * radius * radius;
+}
+double Sphere::getArea() {
+	return 4 * 3.14 * radius * radius;
+}
+int main() {
+	Sphere cir;
+	cir.radius = 3;
+	cout << "구의 부피는 " << cir.getVolume() << endl;
+	cout << "구의 표면적은 " << cir.getArea() << endl;
+	return 0;
+}
