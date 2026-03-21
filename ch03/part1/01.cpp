242f0195a2be4fb1caa1748970cab00552c27a4e
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:51:46 +0900

    Update 01.cpp

--- ch03/part1/01.cpp
+++ ch03/part1/01.cpp
@@ -4,7 +4,6 @@
 // 작성자 : 2500008 권오현
 // **********************************************
 
-
 #include <iostream>
 
 using namespace std;
@@ -23,92 +22,5 @@ int main() {
 	tri.height = 5;
 	cout << "삼각형의 면적은 " << tri.getArea() << endl;
 	cout << sizeof(Triangle);
-	return 0;
-}
-
-
-using namespace std;
-class Sphere {
-public:
-	double radius;
-	double getArea();
-	double getVolume();
-};
-double Sphere::getVolume() {
-	return (4.0/3.0) * 3.14 * radius * radius * radius;
-}
-double Sphere::getArea() {
-	return 4 * 3.14 * radius * radius;
-}
-int main() {
-	Sphere cir;
-	cir.radius = 3;
-	cout << "구의 부피는 " << cir.getVolume() << endl;
-	cout << "구의 표면적은 " << cir.getArea() << endl;
-	return 0;
-}
-
-using namespace std;
-class Rectangle { 
-public:
-	int x;
-	int y;
-	int width;
-	int height;
-	int getArea(); 
-	int getPerimeter();
-	void getCoordinate();
-};
-int Rectangle::getArea() { 
-	return width * height;
-}
-int Rectangle::getPerimeter() {
-	return width + width + height + height;
-}
-void Rectangle::getCoordinate() {
-	cout << "사각형의 우측하단의 좌표는 (" << x + width << "," << y - height << ")" << endl;
-}
-int main() {
-	Rectangle rect;
-	rect.x = 1;
-	rect.y = 2;
-	rect.width = 3;
-	rect.height = 4;
-	cout << "사각형의 면적은 " << rect.getArea() << endl;
-	cout << "사각형의 둘레길이는 " << rect.getPerimeter() << endl;
-	rect.getCoordinate();
-	return 0;
-}
-
-
-using namespace std;
-class Rectangle {
-public:
-	int x;
-	int y;
-	int width;
-	int height;
-	int getArea();
-	int getPerimeter();
-	void getCoordinate();
-};
-int Rectangle::getArea() {
-	return width * height;
-}
-int Rectangle::getPerimeter() {
-	return width + width + height + height;
-}
-void Rectangle::getCoordinate() {
-	cout << "사각형의 우측하단의 좌표는 (" << x + width << "," << y - height << ")" << endl;
-}
-int main() {
-	Rectangle rect;
-	cout << "사각형의 좌측상단좌표(x,y) : ";
-	cin >> rect.x >> rect.y;
-	cout << "사각형의 폭과 높이(width,height) : ";
-	cin >> rect.width >> rect.height;
-	cout << "사각형의 면적은 " << rect.getArea() << endl;
-	cout << "사각형의 둘레길이는 " << rect.getPerimeter() << endl;
-	rect.getCoordinate();
 	return 0;
 }
