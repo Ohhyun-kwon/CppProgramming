7e35ffa5fbc0afe6745a9edfdf7d9a8c91ace601
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:43:15 +0900

    Update 04.cpp

--- ch02/part1/04.cpp
+++ ch02/part1/04.cpp
@@ -1,1 +1,12 @@
 
+
+using namespace std;
+int main(void) {
+    for (int n = 0; n < 4; n++) {
+        for (int m = 0; m <= n; m++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+	return 0;
+}
