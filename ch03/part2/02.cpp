8b768bd690e5cb4887e63fbaa6eb955dd413cce2
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:56:05 +0900

    Update 02.cpp

--- ch03/part2/02.cpp
+++ ch03/part2/02.cpp
@@ -5,3 +5,33 @@
 // **********************************************
 
 #include <iostream>
+
+
+
+
+
+using namespace std;
+
+class Sphere {
+    double radius;
+public:
+    Sphere();
+    Sphere(double radius);
+    double getVolume();
+};
+
+Sphere::Sphere() : Sphere(1) {}
+Sphere::Sphere(double radius) : radius(radius) {}
+
+double Sphere::getVolume() {
+    return (4.0 / 3.0) * 3.14 * radius * radius * radius;
+}
+
+int main() {
+    Sphere sph1;
+    cout << "구의 부피는 " << sph1.getVolume() << endl;
+
+    Sphere sph2(3);
+    cout << "구의 부피는 " << sph2.getVolume() << endl;
+    return 0;
+}
