333374abe1791244b22185be29ce0263bad23d05
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-21 14:43:10 +0900

    Update 03.cpp

--- ch02/part1/03.cpp
+++ ch02/part1/03.cpp
@@ -1,1 +1,16 @@
 
+
+int main(void) {
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 7; j++) {
+			if (i == j || i + j == 6) {
+				std::cout << "*";
+			}
+			else {
+				std::cout << " ";
+			}
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
