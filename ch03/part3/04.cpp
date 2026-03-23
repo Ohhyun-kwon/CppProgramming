6dd3a91dda2d700abf1ce01cbfbdb710c76f2be1
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-23 17:48:39 +0900

    Update 04.cpp

--- ch03/part3/04.cpp
+++ ch03/part3/04.cpp
@@ -1,1 +1,42 @@
+// **********************************************
+// 제 목 : 문제4
+// 날 짜 : 2026년 3월23일
+// 작성자 : 2500008 권오현
+// 질문 : 실습과제4번과 실행결과가 다른 이유를 설명하라.
+// 답 : 
+// **********************************************
 
+#include <iostream>
+
+using namespace std;
+class Sphere {
+public:
+    int radius;
+    Sphere();
+    Sphere(int r);
+    double getVolume();
+    ~Sphere();
+};
+
+Sphere::Sphere() : Sphere(1) {}
+
+Sphere::Sphere(int r) : radius(r) {
+    cout << "반지름 " << radius << "인 구 생성" << endl;
+}
+
+double Sphere::getVolume() {
+    return (4.0 / 3.0) * 3.14 * radius * radius * radius;
+}
+
+Sphere::~Sphere() {
+    cout << "반지름 " << radius << "인 구 소멸" << endl;
+}
+
+Sphere sph1;
+Sphere sph2(3);
+
+int main() {
+    cout << "구의 부피는 " << sph1.getVolume() << endl;
+    cout << "구의 부피는 " << sph2.getVolume() << endl;
+    return 0;
+}
