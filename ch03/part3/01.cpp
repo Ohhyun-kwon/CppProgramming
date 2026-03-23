4551bcd726d899a6d0d66fca07dfeae8a3818305
Ohhyun-kwon <uwq02093@gmail.com>
2026-03-23 17:27:28 +0900

    Create 01.cpp

---
+++ ch03/part3/01.cpp
@@ -0,0 +1,41 @@
+// **********************************************
+// 제 목 : 문제1
+// 날 짜 : 2026년 3월23일
+// 작성자 : 2500008 권오현
+// **********************************************
+
+#include <iostream>
+
+using namespace std;
+
+class Triangle {
+public:
+    int width, height;
+    Triangle();
+    Triangle(int w, int h);
+    double getArea();
+    ~Triangle();
+};
+
+Triangle::Triangle() : Triangle(1, 1) {}
+
+Triangle::Triangle(int w, int h) : width(w), height(h) {
+    cout << "밑변 " << width << " 높이 " << height << "인 삼각형 생성" << endl;
+}
+
+double Triangle::getArea() {
+    return 0.5 * width * height;
+}
+
+Triangle::~Triangle() {
+    cout << "밑변 " << width << " 높이 " << height << "인 삼각형 소멸" << endl;
+}
+
+int main() {
+    Triangle tri1; 
+    cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+
+    Triangle tri2(2, 4);
+    cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+    return 0;
+}
