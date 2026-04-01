5daf938c313f4d357356c1daa0a9198352ecdabb
Ohhyun-kwon <uwq02093@gmail.com>
2026-04-01 17:55:30 +0900

    Update 01.cpp

--- ch04/part1/01.cpp
+++ ch04/part1/01.cpp
@@ -3,3 +3,27 @@
 // 날 짜 : 2026년 4월1일
 // 작성자 : 2500008 권오현
 // **********************************************
+
+#include <iostream>
+
+using namespace std;
+class Triangle {
+private:
+	int width, height;
+public:
+	Triangle() : Triangle(1, 1) {}
+	Triangle(int w, int h) : width(w), height(h) { cout << "폭" << width << ",높이" << height << "인 삼각형 생성" << endl; }
+	~Triangle() { cout << "폭" << width << ",높이" << height << "인 삼각형 소멸" << endl; }
+	void setWidth(int w) { width = w; }
+	void setHeight(int h) { height = h; }
+	double getArea() { return 0.5 * width * height; }
+};
+
+int main(void) {
+	Triangle tri;
+	Triangle* ptr = &tri;
+	tri.setWidth(3);
+	tri.setHeight(5);
+	cout << "삼각형의 면적은 " << ptr->getArea() << endl;
+	return 0;
+}
