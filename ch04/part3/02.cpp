8181c0b303d56c732fc73f03ec8ced607b1bc89a
Ohhyun-kwon <uwq02093@gmail.com>
2026-04-08 10:29:08 +0900

    Create 02.cpp

---
+++ ch04/part3/02.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+Circle::Circle() {
+	radius = 1; cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r; cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	Circle* pArray = new Circle[3];
+	for (int i = 0; i < 3; i++) {
+		cout << pArray[i].getArea() << '\n';
+	}
+	delete[] pArray;
+	return 0;
+}
