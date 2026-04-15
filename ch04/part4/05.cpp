73ad8010d9918305bb3d7385ca0901bf03160e33
Ohhyun-kwon <uwq02093@gmail.com>
2026-04-15 22:18:17 +0900

    Update 05.cpp

--- ch04/part4/05.cpp
+++ ch04/part4/05.cpp
@@ -9,34 +9,39 @@
 
 using namespace std;
 int main() {
-    string url;
-    getline(cin, url);
+    while (true) {
+        string url;
+        cout << "웹 주소 입력>>";
+        getline(cin, url);
 
-    string protocol, host, port = "80", rest = "/";
+        if (url == "stop") break;
 
-    int p = url.find("://");
-    protocol = url.substr(0, p);
+        string protocol, host, port = "80", rest = "/";
 
-    string remain = url.substr(p + 3);
+        int p = url.find("://");
+        protocol = url.substr(0, p);
 
-    int s = remain.find('/');
-    if (s != string::npos) {
-        rest = remain.substr(s);
-        remain = remain.substr(0, s);
+        string remain = url.substr(p + 3);
+
+        int s = remain.find('/');
+        if (s != string::npos) {
+            rest = remain.substr(s);
+            remain = remain.substr(0, s);
+        }
+
+        int c = remain.find(':');
+        if (c != string::npos) {
+            host = remain.substr(0, c);
+            port = remain.substr(c + 1);
+        }
+        else {
+            host = remain;
+        }
+
+        cout << "프로토콜: " << protocol << endl;
+        cout << "호스트: " << host << endl;
+        cout << "포트: " << port << endl;
+        cout << "나머지 부분: " << rest << endl;
     }
-
-    int c = remain.find(':');
-    if (c != string::npos) {
-        host = remain.substr(0, c);
-        port = remain.substr(c + 1);
-    }
-    else {
-        host = remain;
-    }
-
-    cout << "프로토콜: " << protocol << endl;
-    cout << "호스트: " << host << endl;
-    cout << "포트: " << port << endl;
-    cout << "나머지 부분: " << rest << endl;
-    return
+    return 0;
 }
